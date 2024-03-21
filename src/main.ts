0f300ce8dce70c35c723b5f42fbadeac8d9c49cd
xmd5a <zakaru@gmail.com>
2024-03-21 20:47:36 +0100

    simple scene, simple model, simple animation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "./style.css";
 import {
   Engine,
   Scene,
@@ -7,3 +8,29 @@ import {
 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF/2.0";
 import "@babylonjs/core/Helpers/sceneHelpers";
+import model from "./rusty_container.glb";
+
+const canvas = document.querySelector("canvas");
+
+if (!canvas) {
+  throw Error("canvas not found");
+}
+
+const engine = new Engine(canvas, true);
+const scene = new Scene(engine);
+const camera = new ArcRotateCamera("camera", 1, 1, 20, new Vector3(0, 2, 0));
+
+camera.attachControl(canvas, true);
+scene.createDefaultEnvironment();
+
+scene.createDefaultEnvironment();
+
+engine.runRenderLoop(() => {
+  scene.render();
+});
+
+SceneLoader.ImportMeshAsync(null, model, "", scene).then((result) => {
+  scene.onBeforeRenderObservable.add(() => {
+    result.meshes[0].rotate(new Vector3(0, 1, 0), 0.01);
+  });
+});
