0b4183d19540cd081ecbde091c190bcfbeae4a3e
xmd5a <zakaru@gmail.com>
2024-03-21 20:03:43 +0100

    initiate app

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import {
+  Engine,
+  Scene,
+  Vector3,
+  SceneLoader,
+  ArcRotateCamera,
+} from "@babylonjs/core";
+import "@babylonjs/loaders/glTF/2.0";
+import "@babylonjs/core/Helpers/sceneHelpers";
